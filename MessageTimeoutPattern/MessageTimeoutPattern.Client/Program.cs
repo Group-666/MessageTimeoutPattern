a7b81e33bb72103ae8e58cf01a6b844614aee368
Nicholas Hansen <nicholasfhansen@gmail.com>
2017-03-29 16:28:12 +0200

    initial project structure

---
+++ MessageTimeoutPattern/MessageTimeoutPattern.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageTimeoutPattern.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MessageTimeoutPattern/MessageTimeoutPattern.MessageBroker/Controllers/MessageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageTimeoutPattern.MessageBroker.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        // POST api/message
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+    }
+}
